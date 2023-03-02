a1bea7e914f4c8d2948bef3f5b69e149b0a06fb3
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-03-02 19:22:25 -0300

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,6 +1,6 @@
 /*Escreva um programa que declare um array de inteiros com 5 elementos e um ponteiro para o primeiro elemento do array. 
-Em seguida, preencha o array com valores informados pelo usu·rio e imprima esses valores utilizando o ponteiro. 
-Ao final, mostre em tela cada valor armazenado e seu respectivo endereÁo de memÛria. 
+Em seguida, preencha o array com valores informados pelo usu√°rio e imprima esses valores utilizando o ponteiro. 
+Ao final, mostre em tela cada valor armazenado e seu respectivo endere√ßo de mem√≥ria. 
 */
 
 #include <stdio.h>
@@ -20,7 +20,7 @@ int main(){
 	
     for(int j = 0; j < 5; j++){
     	printf("\nValor: %d\n", *(pont + j));
-    	printf("EndereÁo: %p\n", pont + j);
+    	printf("Endere√ßo: %p\n", pont + j);
 	}
 	
 	
