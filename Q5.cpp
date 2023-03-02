63748f4ecafbf736b6d577d7b19134353dc58553
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-03-02 00:06:21 -0300

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,3 +1,8 @@
+/* Escreva um programa que declare uma variável real e um ponteiro para essa variável. Em seguida, 
+preencha a variável com um valor real informado pelo usuário e utilize o ponteiro para calcular o dobro desse valor.
+ Por fim, utilize o ponteiro para imprimir na tela o valor original e o dobro calculado.
+*/
+
 #include <stdio.h>
 #include <locale.h>
 
@@ -7,15 +12,14 @@ int main() {
     float numero;
     float *pont_numero;
 
-    printf("Digite um n�mero real: ");
+    printf("Digite um número real: ");
     scanf("%f", &numero);
 
     pont_numero = &numero;
 
     *pont_numero = *pont_numero * 2; 
 
-    printf("\nO n�mero original � %.2f e seu dobro � %.2f.\n", numero/2, *pont_numero); 
+    printf("\nO número original é %.2f e seu dobro é %.2f.\n", numero/2, *pont_numero); 
 
     return 0;
 }
-
